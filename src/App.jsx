d7dc7eb019b5c1b1dd9e3860719e3a8bf20e4f6c
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-07-13 17:09:27 +0200

    update app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+import data from './data';
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  return (
+    <section className="section-center">
+      <h4>tired of boring lorem ipsum?</h4>
+      <form className="lorem-form">
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min="1"
+          max="8"
+          step="1"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default App;
